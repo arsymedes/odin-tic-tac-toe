c0371a39f6c4cc592be19e5f89ce44437085bde5
arsyahmad12 <arsyahmad14@gmail.com>
2022-06-12 00:56:53 +0700

    Finished End Condition Check

--- javascript.js
+++ javascript.js
@@ -12,13 +12,7 @@ const gameBoard = (() => {
     board.innerHTML = boardContent;
 
     document.querySelectorAll(".empty").forEach((div) => {
-      div.addEventListener("click", () => {
-        let mark = gameEngine.currentPlayer().markNum;
-        let index = div.dataset.num
-        grid[index] = mark;
-        gameEngine.turn++
-        displayGrid()
-      })
+      div.addEventListener("click", player1.addMark)
     })
   }
 
@@ -32,7 +26,25 @@ const Player = (mark) => {
   let markNum = 0
   if (mark == "cross") markNum = 1;
 
-  const addMark = () => {
+  function addMark() {
+    let mark = gameEngine.currentPlayer().markNum;
+    let index = this.dataset.num;
+    gameBoard.grid[index] = mark;
+    console.log(gameEngine.endCondition(gameBoard.grid))
+    gameBoard.displayGrid()
+  }
+
+  return {
+    addMark,
+    markNum,
+  }
+};
+
+const Computer = (mark) => {
+  let markNum = 0
+  if (mark == "cross") markNum = 1;
+
+  function addMark() {
     
   }
 
@@ -47,17 +59,32 @@ const gameEngine = (() => {
   
   function currentPlayer() {
     if (turn % 2) {
-      turn++
-      return Player("cross")
+      turn++;
+      return player1;
     } else if (!(turn % 2)) {
-      turn++
-      return Player("circle")
+      turn++;
+      return player2;
     }
   }
 
+  function endCondition(list) {
+    for (let i = 0; i < 3; i++) {
+      if (!isNaN(parseInt(list[3*i])) && list[3*i] === list[3*i+1] && list[3*i+1] === list[3*i+2]) return true;
+      if (!isNaN(parseInt(list[i])) && list[i] === list[i+3] && list[i+3] === list[i+6]) return true;
+    };
+    if (!isNaN(parseInt(list[0])) && list[0] === list[4] && list[4] === list[8]) return true;
+    if (!isNaN(parseInt(list[2])) && list[2] === list[4] && list[4] === list[6]) return true;
+    return false
+  }
+  
+
   return {
     currentPlayer,
+    endCondition,
   }
 })();
 
+const player1 = Player("cross")
+const player2 = Player("circle")
 gameBoard.displayGrid()
+
